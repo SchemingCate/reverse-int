8837c94bf9b93baccb3f5e6c1b092ed03844c9b1
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-01-25 03:36:54 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const stringNumber = n.toString();
+    let reversedStringNumber = "";
+    for (let i = stringNumber.length - 1; i >= 0; i--) {
+        if (isNaN(stringNumber[i])) {
+            continue;
+        }
+        reversedStringNumber += stringNumber[i];
+    }
+    return Number(reversedStringNumber);
+};
